b4e6103568d00d8836f2a1a84d63089693856be1
swathim5309-droid <swathim5309@gmail.com>
2026-01-12 11:14:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ except Exception as e:
     print("❌ Sensor model load failed:", repr(e))
 
 ACTION_MAP = {0: "Normal", 1: "Spoofing"}  # Example mapping
-SENSOR_REQUIRED_FEATURES = ['speed_kmh
-','acceleration_mps2','lane_deviation','obstacle_distance','traffic_density',]  # adjust to your dataset
+SENSOR_REQUIRED_FEATURES = ['speed_kmh','acceleration_mps2','lane_deviation','obstacle_distance','traffic_density']
 
 @app.get("/")
 def health():
